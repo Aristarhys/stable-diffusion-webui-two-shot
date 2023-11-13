e6937f786408c28c545c49d628816c312f6f52d3
Artem Bashev <artem.bashev@gmail.com>
2023-11-13 21:53:10 +0300

    Backported infotext_send_params by space-nuko

--- scripts/two_shot.py
+++ scripts/two_shot.py
@@ -252,6 +252,9 @@ class Script(scripts.Script):
             step = int(step)
         return raw_params.get('divisions', divisions), raw_params.get('positions', positions), raw_params.get('weights', weights), step
 
+    def do_copy(self, raw_divisions: str, raw_positions: str, raw_weights: str, raw_end_at_step: int) -> str:
+        return f"divisions={raw_divisions} positions={raw_positions} weights={raw_weights} end at step={raw_end_at_step}"
+
     def ui(self, is_img2img):
         process_script_params = []
         id_part = "img2img" if is_img2img else "txt2img"
@@ -478,9 +481,12 @@ class Script(scripts.Script):
                         visualize_button.click(fn=self.do_visualize, inputs=[divisions, positions, weights], outputs=[visual_regions])
 
                         extra_generation_params = gr.Textbox(label="Extra generation params")
-                        apply_button = gr.Button(value="Apply")
 
-                        apply_button.click(fn=self.do_apply, inputs=[extra_generation_params, divisions, positions, weights, end_at_step], outputs=[divisions, positions, weights, end_at_step])
+                        with gr.Row():
+                            apply_button = gr.Button(value="Apply", variant="primary")
+                            apply_button.click(fn=self.do_apply, inputs=[extra_generation_params, divisions, positions, weights, end_at_step], outputs=[divisions, positions, weights, end_at_step])
+                            copy_button = gr.Button(value="Copy")
+                            copy_button.click(fn=self.do_copy, inputs=[divisions, positions, weights, end_at_step], outputs=[extra_generation_params])
 
                     def select_twosoht_tab(tab_id):
                         self.selected_twoshot_tab = tab_id
@@ -497,6 +503,7 @@ class Script(scripts.Script):
         self.infotext_fields = [
             (extra_generation_params, "Latent Couple")
         ]
+        self.paste_field_names = ["Latent Couple"]
         process_script_params.append(enabled)
         process_script_params.append(divisions)
         process_script_params.append(positions)
